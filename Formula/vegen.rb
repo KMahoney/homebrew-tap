cc80f9778a9c01086791d4c60b53a0cc976380e7
Kevin Mahoney <kevin@kevinmahoney.co.uk>
2025-10-15 15:29:30 +0000

    vegen: update 0.3.0 bottle.

--- Formula/vegen.rb
+++ Formula/vegen.rb
@@ -6,9 +6,9 @@ class Vegen < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/KMahoney/homebrew-tap/releases/download/vegen-0.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "63a59f42ed8dd4df95998719b3d075b8fb3289f2825dcbdeda70ed9797531483"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a303bea80c31c3c02e43d81927f9723094fc4aadf7af8c35a09120521253c39f"
+    root_url "https://github.com/KMahoney/homebrew-tap/releases/download/vegen-0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "0898a3a5248948f3c35a60edd32805fcc58ef783bac7468591f48bc9a45a42a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dadd05c896e85b1c90c6a312560a419613d9165650744410227e3e76c359b2c8"
   end
 
   depends_on "rust" => :build
